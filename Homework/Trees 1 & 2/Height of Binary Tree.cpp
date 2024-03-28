5825fdbaf93183085a9e41b8d136fe075e864680
Animesh Sinha <animeshsinha1903@gmail.com>
2024-03-28 18:13:01 +0530

    Completed Trees 1 & 2

--- Homework/Trees 1 & 2/Height of Binary Tree.cpp
+++ Homework/Trees 1 & 2/Height of Binary Tree.cpp
@@ -1,25 +1,3 @@
-/************************************************************
-
-    Following is the TreeNode class structure
-
-    template <typename T>
-    class TreeNode
-    {
-    public:
-        T val;
-        TreeNode<T> *left;
-        TreeNode<T> *right;
-
-        TreeNode(T val)
-        {
-            this->val = val;
-            left = NULL;
-            right = NULL;
-        }
-    };
-
-************************************************************/
-
 int heightOfBinaryTree(TreeNode<int> *root)
 {
 	if (!root)
